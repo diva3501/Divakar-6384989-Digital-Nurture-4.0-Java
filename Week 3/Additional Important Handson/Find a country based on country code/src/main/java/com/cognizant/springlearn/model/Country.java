6d6399a7b0b44d7c46966a062bde875f9a91dfe5
Divakar G <divakar3501@gmail.com>
2025-07-03 17:59:34 +0530

    Find a country based on country code

---
+++ Week 3/Additional Important Handson/Find a country based on country code/src/main/java/com/cognizant/springlearn/model/Country.java
@@ -0,0 +1,35 @@
+package com.cognizant.springlearn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+    @Id
+    private String code;
+    private String name;
+
+    // Getters & Setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country [code=" + code + ", name=" + name + "]";
+    }
+}
