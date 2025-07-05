88e4e22c06887f78d558940b95fb30a4b29a13f2
Divakar G <divakar3501@gmail.com>
2025-07-05 22:50:57 +0530

    Exercise 9 Creating a Spring Boot Application

---
+++ Week 3/Additional Important Handson/Exercise 9 Spring Boot/LibraryManagement/src/main/java/com/example/LibraryManagement/controller/BookController.java
@@ -0,0 +1,55 @@
+package com.example.LibraryManagement.controller;
+
+import com.example.LibraryManagement.model.Book;
+import com.example.LibraryManagement.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Create a new book
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Get all books
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Get book by ID
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id);
+    }
+
+    // Update book
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+        return bookRepository.findById(id).map(book -> {
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setYear(updatedBook.getYear());
+            return bookRepository.save(book);
+        }).orElseGet(() -> {
+            updatedBook.setId(id);
+            return bookRepository.save(updatedBook);
+        });
+    }
+
+    // Delete book
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
